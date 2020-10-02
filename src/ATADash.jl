a739ef902ae70ead5a1b00896b05b8a725ffde13
giadasp <giada.spaccapanico@gmail.com>
2020-10-03 01:31:15 +0200

    initial commit

--- src/ATADash.jl
+++ src/ATADash.jl
@@ -1,5 +1,17 @@
 module ATADash
+import JLD2
+import Dash
+import DashHtmlComponents
+import DashCoreComponents
 
-greet() = print("Hello World!")
+using Requires
+
+function __init__()
+    @require ATA = "a8b2d192-9814-11e9-3a67-ff0161457e0c" begin
+        include("dash.jl")
+    end
+end
+
+export ATA_app
 
 end # module
